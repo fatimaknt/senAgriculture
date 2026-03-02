9bdd4f120f42b9b4ae641292191de9a7880c085f
kanoutef163 <kanoutef163@gmail.com>
2026-03-02 13:17:13 +0100

    Premier commit

---
+++ SenAgriculture/AppSenAgriculture/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppSenAgriculture
+{
+    public partial class frmConnexion : Form
+    {
+        public frmConnexion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSeConnecter_Click(object sender, EventArgs e)
+        {
+            frmMDI f=new frmMDI();
+            f.Show();
+            this.Hide();
+        }
+    }
+}
