3bcf8e6f10a32c3e3ee6b3c95e27eca7b1edfd33
gat0sy <gat0sy.pro@gmail.com>
2026-03-03 14:42:24 +0000

    ajout du helper Crypto & edition the Form1.cs

--- SenAgriculture/AppSenAgriculture/Form1.cs
+++ SenAgriculture/AppSenAgriculture/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AppSenAgriculture.Models;
+using AppSenAgriculture.Helpers;
 
 namespace AppSenAgriculture
 {
@@ -24,9 +26,36 @@ namespace AppSenAgriculture
 
         private void btnSeConnecter_Click(object sender, EventArgs e)
         {
-            frmMDI f=new frmMDI();
+            //----ANCIEN CODE-----
+            //frmMDI f=new frmMDI();
+            //f.Show();
+            //this.Hide();
+            string identifiant = txtIdentifiant.Text;
+            string password = txtMotDePasse.Text;
+
+            string hashedPassword = Crypto.HashMd5(password);
+            
+            using (BdSenAgricultureContext db = new BdSenAgricultureContext())
+    {
+        //Attention
+        //Il faut que le mot de passe soit un hash au préalable sinon la connexion ne pourra pas marcher
+        //Example:  user.MotDePasseUtilisateur = Crypto.HashMd5(plainPassword);
+        var user = db.utilisateurs
+                     .FirstOrDefault(u => 
+                         u.IdentifiantUtilisateur == identifiant &&
+                         u.MotDePasseUtilisateur == hashedPassword);
+
+        if (user != null)
+        {
+            frmMDI f = new frmMDI();
             f.Show();
             this.Hide();
+        }
+        else
+        {
+            MessageBox.Show("Identifiant ou mot de passe incorrect !");
+        }
+    }
         }
     }
 }
